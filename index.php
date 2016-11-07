4b063cf6071005dd24819c313b4090886c706eff
cstchrisngo <cngo@csumb.edu>
2016-11-07 21:00:11 +0000

    SQL code to manipulate database

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+include '../../includes/dbConnection.php';
+$conn = getDatabaseConnection("bestbuy");
+
+function listProducts() {
+    global $conn;
+    $sql = "SELECT productName, type, price
+            FROM products
+            ORDER BY productName";
+            
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $products;
+}
+
+function getDepartments() {
+    global $conn;
+    $sql = "SELECT type
+            FROM department
+            ORDER BY type";
+    
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+    $products = $statement->fetch(PDO::FETCH_ASSOC);
+       
+}
+
+function getProdByDept(){
+    
+}
+
+function getEmployee() {
+    
+}
+
+?>
+ 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team Project: Bestbuy Store</title>
+        
+    </head>
+</html>
