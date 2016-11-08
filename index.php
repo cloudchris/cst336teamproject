faf133f462711ae261aa9ca79603b3306e321e05
cstchrisngo <cngo@csumb.edu>
2016-11-08 23:22:52 +0000

    updated stuff almostttt

--- index.php
+++ index.php
@@ -10,22 +10,33 @@ if($conn->connect_error){
 function listProducts() {
     global $conn;
     $sql = "SELECT *
-            FROM products";
+            FROM products
+            WHERE 1";
             
-    if (isset($_GET['submit'])){
-        //if form has been submitted
+ 
+            if (isset($_GET['search'])){
+            //form has been submitted
 
-        $namedParameters = array();    
-        
-        if(!empty($_GET['productName'])){
-                        //name has been selected
-                        
-                        $sql = $sql . " AND productName = :productName";
-                        
-                        $namedParameters[':productName'] = $_GET['productName'];
-                    }
-        
-        if(isset($_GET['Submit'])) {
+                $namedParameters = array();
+                
+                if (!empty($_GET['productName'])){
+                    $sql = $sql . " AND productName LIKE  :productName "; //using Named Parameters to prevent SQL Injection
+                   
+                    $namedParameters[':productName'] = "%" . $_GET['productName'] . "%";
+                   
+                }
+           
+            if($_GET['filterType'] == "Cell Phones") {
+                $sql .= " AND type = 'Cell Phone'  ";
+            } else if($_GET['filterType'] == "Audio") {
+                $sql .= " AND type = 'Audio'";
+            } else if($_GET['filterType'] == "priceLow") {
+                $sql .= " ORDER BY price ASC";
+            } else if($_GET['filterType'] == "priceHigh") {
+                $sql .= " ORDER BY type DESC";
+            }
+            
+                
             if($_GET['orderBy'] == "nameASC") {
                 $sql .= " ORDER BY productName ASC";
             } else if($_GET['orderBy'] == "nameDSC") {
@@ -35,27 +46,27 @@ function listProducts() {
             } else if($_GET['orderBy'] == "priceHigh") {
                 $sql .= " ORDER BY price DESC";
             }
-        }
-    }
+        
+        }   
+        
             
-     $statement= $conn->prepare($sql); 
-      $statement->execute($namedParameters); //Always pass the named parameters, if any
-      $records = $statement->fetchALL(PDO::FETCH_ASSOC);
+     $statement= $conn->prepare($sql);
+     $statement->execute($namedParameters); //Always pass the named parameters, if any
+     $records = $statement->fetchALL(PDO::FETCH_ASSOC);
    
     foreach($records as $product) {
-        echo $product
-        ['productName'] . " - ". $product['type'] .  " - ". $product['price'] . "<br/> ";
+        echo "<input type='checkbox' name='cart[]'    value =" . $product['productId'] . ">";
+        echo $product['productName'] . " - ". $product['type'] .  " - ". $product['price'];
+        echo "<a href='description.php?productId=".$product['productId']."'>";
+        echo ' <button type="button" class="btn btn-default btn-lg">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Description
+            </button> ';
+        echo "</a>";
+        echo "<br />";
     }
-   
-    return $products;
     
-    foreach($products as $record) {
-          echo "<input type='checkbox' name='cart[]'    value =" . $record['productName'] . ">";
-          echo $record['productName'] . "<br/> ";
-      }
-      
-      echo 'input type="submit" name="loginForm"'; //may need the < > still??
 }
+
 
 function getDepartments() {
     global $conn;
@@ -120,39 +131,41 @@ function getEmployee() {
         <main>
             <h1>Welcome to Bestbuy!</h1>
             
-            
-            <form method="POST">
+            <form method="GET">
                 
-                <input Type="text" name ="productName" placeholder ="Product Name" >
-                <input type="submit" name ="submit" value="Search"/>
-                <h2><strong>Sort Products By: </strong></h2>
+                <input type="text" name ="productName" placeholder ="Product Name"/>
+                
+
+                <h3><strong>Sort Products By: </strong></h3>
                     <select name="orderBy">
                         <option value="nameASC">Product(A-Z)</option>
                         <option value="nameDSC">Product(Z-A)</option>
                         <option value="priceLow">Price: Low to Highest</option>
                         <option value="priceHigh">Price: High to Lowest</option>
                     </select>
-                
+            
                 <br /><br />    
-                <h2><strong>Filter Products By Type: </strong></h2>
+                <h2><strong>Filter Products By Type: </strong></h3>
                         <input type="radio" name="filterType" value="Cell Phones" id="Cell"/><label for="Cell">Cell Phones</label>
                         <input type="radio" name="filterType" value="Audio" id="Aud"/><label for="Aud">Audio</label>
-                        <input type="radio" name="filterType" value="cameras" id="Cam"/><label for="Cam">Cameras</label>
+                        <input type="radio" name="filterType" value="camera" id="Cam"/><label for="Cam">Cameras</label>
                         <input type="radio" name="filterType" value="Computers" id="Comp"/><label for="Comp">Computers</label>
                         <input type="radio" name="filterType" value="Mus&Mov" id="M&M"/><label for="M&M">Music & Movies</label>
                         <input type="radio" name="filterType" value="games" id="Games"/><label for="Game">Video Games</label>
                         <input type="radio" name="filterType" value="Appliances" id="App"/><label for="App">Appliances</label>
                         <input type="radio" name="filterType" value="health&fit" id="H&F"/><label for="H&F">Health & Fitness</label>
                  <br />
-                  <input type="submit" name ="submit" value="Search"/><br/>
+                  <input type="submit" name ="search" value="Search"/><br/>
 
             </form>
-                     
-                 <?=listProducts()?>
-                 <?=getDepartments()?>
-                 <?=getEmployee()?>
-                 <?=getProdByDept()?>
-              
+            
+            <br /><hr><br />
+             <?php
+                listProducts();
+            
+                echo "<td>" . $product['productName'] . " </td> <td> " . "$" . $product['price'] . "</td> ";
+                echo "[Add to Cart] <br/>";
+            ?>
 
 
         </main>
